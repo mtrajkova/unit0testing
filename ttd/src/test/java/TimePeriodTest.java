f8db5767aad1b32b88ca975ec82b377a68f6c1e2
Marija Trajkova <tmarija2008@yahoo.com>
2019-07-29 15:09:27 +0200

    Write unit tests for TimePeriod class

--- ttd/src/test/java/TimePeriodTest.java
+++ ttd/src/test/java/TimePeriodTest.java
@@ -1,10 +1,83 @@
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class TimePeriodTest {
+    private TimePeriod timePeriodA;
+    SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy");
+    TimePeriod timePeriodB;
+
+    @Before
+    public void setUp() throws ParseException {
+        String startA = "01/01/2019";
+        String endA = "31/12/2019";
+        timePeriodA = new TimePeriod(dateFormatter.parse(startA), dateFormatter.parse(endA));
+    }
 
     @Test
-    public void overlapsWith() {
+    public void dateAcontainsDateB() throws ParseException {
+        String startB = "05/05/2019";
+        String endB = "30/12/2019";
+        timePeriodB = new TimePeriod(dateFormatter.parse(startB), dateFormatter.parse(endB));
+
+        Assert.assertTrue(timePeriodA.overlapsWith(timePeriodB));
     }
+
+    @Test
+    public void dateBcontainsDateA() throws ParseException {
+        String startB = "01/05/2018";
+        String endB = "30/12/2020";
+        timePeriodB = new TimePeriod(dateFormatter.parse(startB), dateFormatter.parse(endB));
+
+        Assert.assertTrue(timePeriodA.overlapsWith(timePeriodB));
+    }
+
+    @Test
+    public void dateBinteractsDateA() throws ParseException {
+        String startB = "01/05/2018";
+        String endB = "30/12/2019";
+        timePeriodB = new TimePeriod(dateFormatter.parse(startB), dateFormatter.parse(endB));
+
+        Assert.assertTrue(timePeriodA.overlapsWith(timePeriodB));
+    }
+
+    @Test
+    public void dateAinteractsDateB() throws ParseException {
+        String startB = "05/05/2019";
+        String endB = "01/01/2020";
+        timePeriodB = new TimePeriod(dateFormatter.parse(startB), dateFormatter.parse(endB));
+
+        Assert.assertTrue(timePeriodA.overlapsWith(timePeriodB));
+    }
+
+    @Test
+    public void dateAequalsDateB() throws ParseException {
+        String startB = "01/01/2019";
+        String endB = "31/12/2019";
+        timePeriodB = new TimePeriod(dateFormatter.parse(startB), dateFormatter.parse(endB));
+
+        Assert.assertTrue(timePeriodA.overlapsWith(timePeriodB));
+    }
+
+    @Test
+    public void endOfDateAequalsStartOfDateB() throws ParseException {
+        String startB = "31/12/2019";
+        String endB = "31/12/2019";
+        timePeriodB = new TimePeriod(dateFormatter.parse(startB), dateFormatter.parse(endB));
+
+        Assert.assertTrue(timePeriodA.overlapsWith(timePeriodB));
+    }
+
+    @Test
+    public void dateANotOverlappingWithDateB() throws ParseException {
+        String startB = "01/01/2018";
+        String endB = "30/10/2018";
+        timePeriodB = new TimePeriod(dateFormatter.parse(startB), dateFormatter.parse(endB));
+
+        Assert.assertFalse(timePeriodA.overlapsWith(timePeriodB));
+    }
+
 }
